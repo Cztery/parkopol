e87fea5b8fb26fbe94f272a9f0ea24956eeff2be
Marcin Kurkowski <kurkowski.mrcn@gmail.com>
2018-11-09 15:31:33 +0100

    at least runs

--- parkopol2/park_unit.cpp
+++ parkopol2/park_unit.cpp
@@ -1,13 +1,19 @@
 #include "pch.h"
 #include "park_unit.h"
+//#include <cstring>
+//#include <vector>
+
 
 
 park_unit::park_unit()
 {
-	m_parent = parent;
-	if (parent) m_depthLevel = parent->m_depthLevel + 1;
+	m_parent = NULL;
 }
 
+park_unit::park_unit(park_unit* parent)
+{
+	if (parent) m_depthLevel = parent->m_depthLevel + 1;
+}
 
 park_unit::~park_unit()
 {
@@ -15,43 +21,67 @@ park_unit::~park_unit()
 	m_children.clear();
 }
 
-
-class park_unit
+void park_unit::printID()
 {
-	//private:
-public:
-	park_unit *m_parent;
-	std::vector <park_unit *> m_children;
-	int m_depthLevel;
-	string m_unitname;
-	int m_childId;
-	//public:
-	park_unit(park_unit *parent = NULL)
-	{
-		m_parent = parent;
-		if (parent) m_depthLevel = parent->m_depthLevel + 1;
-	}
-	void printId()
-	{
-		if (m_parent) m_parent->printId();
-		cout << m_childId << ".";
-	}
-	void add_child_unit()
-	{
+	if (m_parent) m_parent->printID();
+	cout << m_childId << ".";
+}
+
+void park_unit::addChildUnit()
+{
 		m_children.push_back(new park_unit(this));
 		m_children.back()->m_childId = m_children.size();
-		printId();
-		cout << " ->\t Adding new child no" << m_children.size() << endl;
-	}
-	void remove_child_unit(int childID)
-	{
-		m_children.erase(m_children.begin() + childID);
-	}
-	~park_unit()
-	{
-		for (auto &i : m_children) delete i;
-		m_children.clear();
-	}
+		printID();
+		cout << " ->\t Adding new child no" << m_children.size() << endl;;
+}
+
+void park_unit::removeChildUnit(int childID)
+{
+}
+
+park_unit * park_unit::getChild(int i)
+{
+	return m_children[i];
+}
 
 
-};
+
+//class park_unit
+//{
+//	//private:
+//public:
+//	park_unit *m_parent;
+//	std::vector <park_unit *> m_children;
+//	int m_depthLevel;
+//	string m_unitname;
+//	int m_childId;
+//	//public:
+//	park_unit(park_unit *parent = NULL)
+//	{
+//		m_parent = parent;
+//		if (parent) m_depthLevel = parent->m_depthLevel + 1;
+//	}
+//	void printId()
+//	{
+//		if (m_parent) m_parent->printId();
+//		cout << m_childId << ".";
+//	}
+//	void add_child_unit()
+//	{
+//		m_children.push_back(new park_unit(this));
+//		m_children.back()->m_childId = m_children.size();
+//		printId();
+//		cout << " ->\t Adding new child no" << m_children.size() << endl;
+//	}
+//	void remove_child_unit(int childID)
+//	{
+//		m_children.erase(m_children.begin() + childID);
+//	}
+//	~park_unit()
+//	{
+//		for (auto &i : m_children) delete i;
+//		m_children.clear();
+//	}
+//
+//
+//};
--- parkopol2/park_unit.h
+++ parkopol2/park_unit.h
@@ -1,18 +1,30 @@
 #pragma once
-
+using namespace std;
+#include <vector>
+#include <iostream>
 
 class park_unit
 {
-public:
+private:
+	///tree - specific elements:
 	park_unit *m_parent;
-	std::vector <park_unit *> m_children;
+	
 	int m_depthLevel;
+
 	string m_unitname;
 	int m_childId;
 
+
+public:
+	vector <park_unit *> m_children;
+
 	park_unit();
+	park_unit(park_unit* parent);
 	~park_unit();
 
-
+	void printID();
+	void addChildUnit();
+	void removeChildUnit(int childID);
+	park_unit* getChild(int i);
 };
 
